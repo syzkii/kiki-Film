f1be08537928fd10c046ac09d5f3ba17a6d71274
syzkii <rifkiardianakbar.raa@gmail.com>
2022-04-28 19:54:50 +0700

    Add: Gambar

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -3,6 +3,15 @@ import threeFiveFiveImage from "../assets/images/card/355.jpg"
 import moneyHeist from "../assets/images/card/Moneyheist.jpg"
 import eternals from "../assets/images/card/Eternals.jpg"
 import geezAnn from "../assets/images/card/Geezann.jpg"
+import inception from "../assets/images/card/Inception.jpg"
+import jumanji from "../assets/images/card/Jumanjo.jpg"
+import storks from "../assets/images/card/Storks.jpg"
+import theWithcer from "../assets/images/card/Thewitcher.jpg"
+import valhalla from "../assets/images/card/Valhalla.jpg"
+import kagetNikah from "../assets/images/card/Kagetnikah.jpg"
+import space from "../assets/images/card/Space.jpg"
+import allOfDeath from "../assets/images/card/Allofdeath.jpg"
+
 
 const Trending = () => {
     return (
@@ -28,7 +37,7 @@ const Trending = () => {
                 <Image src={moneyHeist} alt="Image 355" className="image"/>
                 <div className="bg-dark">
                   <div className="p-2 m-1 text-white">
-                    <Card.Title className="text-center">355</Card.Title>
+                    <Card.Title className="text-center">Money Heist</Card.Title>
                     <Card.Text className="text-left">
                       This is a wider card with supporting text below as a natural. This content is a little bit longer.
                       </Card.Text>
@@ -42,7 +51,7 @@ const Trending = () => {
                 <Image src={eternals} alt="Image 355" className="image"/>
                 <div className="bg-dark">
                   <div className="p-2 m-1 text-white">
-                    <Card.Title className="text-center">355</Card.Title>
+                    <Card.Title className="text-center">Eternals</Card.Title>
                     <Card.Text className="text-left">
                       This is a wider card with supporting text below as a natural. This content is a little bit longer.
                       </Card.Text>
@@ -56,7 +65,7 @@ const Trending = () => {
                 <Image src={geezAnn} alt="Image 355" className="image"/>
                 <div className="bg-dark">
                   <div className="p-2 m-1 text-white">
-                    <Card.Title className="text-center">355</Card.Title>
+                    <Card.Title className="text-center">Geez & Ann</Card.Title>
                     <Card.Text className="text-left">
                       This is a wider card with supporting text below as a natural. This content is a little bit longer.
                       </Card.Text>
@@ -67,10 +76,10 @@ const Trending = () => {
               </Col>
               <Col md={4} className="movieWrapper">
                 <Card className="filmImage">
-                <Image src={eternals} alt="Image 355" className="image"/>
+                <Image src={storks} alt="Image 355" className="image"/>
                 <div className="bg-dark">
                   <div className="p-2 m-1 text-white">
-                    <Card.Title className="text-center">355</Card.Title>
+                    <Card.Title className="text-center">Storks</Card.Title>
                     <Card.Text className="text-left">
                       This is a wider card with supporting text below as a natural. This content is a little bit longer.
                       </Card.Text>
@@ -81,10 +90,94 @@ const Trending = () => {
               </Col>
               <Col md={4} className="movieWrapper">
                 <Card className="filmImage">
-                <Image src={eternals} alt="Image 355" className="image"/>
+                <Image src={inception} alt="Image 355" className="image"/>
                 <div className="bg-dark">
                   <div className="p-2 m-1 text-white">
-                    <Card.Title className="text-center">355</Card.Title>
+                    <Card.Title className="text-center">Inception</Card.Title>
+                    <Card.Text className="text-left">
+                      This is a wider card with supporting text below as a natural. This content is a little bit longer.
+                      </Card.Text>
+                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                  </div>
+                </div>
+                </Card>
+              </Col>
+              <Col md={4} className="movieWrapper">
+                <Card className="filmImage">
+                <Image src={theWithcer} alt="Image 355" className="image"/>
+                <div className="bg-dark">
+                  <div className="p-2 m-1 text-white">
+                    <Card.Title className="text-center">The Witcher</Card.Title>
+                    <Card.Text className="text-left">
+                      This is a wider card with supporting text below as a natural. This content is a little bit longer.
+                      </Card.Text>
+                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                  </div>
+                </div>
+                </Card>
+              </Col>
+              <Col md={4} className="movieWrapper">
+                <Card className="filmImage">
+                <Image src={valhalla} alt="Image 355" className="image"/>
+                <div className="bg-dark">
+                  <div className="p-2 m-1 text-white">
+                    <Card.Title className="text-center">Valhalla</Card.Title>
+                    <Card.Text className="text-left">
+                      This is a wider card with supporting text below as a natural. This content is a little bit longer.
+                      </Card.Text>
+                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                  </div>
+                </div>
+                </Card>
+              </Col>
+              <Col md={4} className="movieWrapper">
+                <Card className="filmImage">
+                <Image src={jumanji} alt="Image 355" className="image"/>
+                <div className="bg-dark">
+                  <div className="p-2 m-1 text-white">
+                    <Card.Title className="text-center">Jumanji</Card.Title>
+                    <Card.Text className="text-left">
+                      This is a wider card with supporting text below as a natural. This content is a little bit longer.
+                      </Card.Text>
+                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                  </div>
+                </div>
+                </Card>
+              </Col>
+              <Col md={4} className="movieWrapper">
+                <Card className="filmImage">
+                <Image src={kagetNikah} alt="Image 355" className="image"/>
+                <div className="bg-dark">
+                  <div className="p-2 m-1 text-white">
+                    <Card.Title className="text-center">Kaget Nikah</Card.Title>
+                    <Card.Text className="text-left">
+                      This is a wider card with supporting text below as a natural. This content is a little bit longer.
+                      </Card.Text>
+                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                  </div>
+                </div>
+                </Card>
+              </Col>
+              <Col md={4} className="movieWrapper">
+                <Card className="filmImage">
+                <Image src={space} alt="Image 355" className="image"/>
+                <div className="bg-dark">
+                  <div className="p-2 m-1 text-white">
+                    <Card.Title className="text-center">Space</Card.Title>
+                    <Card.Text className="text-left">
+                      This is a wider card with supporting text below as a natural. This content is a little bit longer.
+                      </Card.Text>
+                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                  </div>
+                </div>
+                </Card>
+              </Col>
+              <Col md={4} className="movieWrapper">
+                <Card className="filmImage">
+                <Image src={allOfDeath} alt="Image 355" className="image"/>
+                <div className="bg-dark">
+                  <div className="p-2 m-1 text-white">
+                    <Card.Title className="text-center">All Of Us Are Death</Card.Title>
                     <Card.Text className="text-left">
                       This is a wider card with supporting text below as a natural. This content is a little bit longer.
                       </Card.Text>
